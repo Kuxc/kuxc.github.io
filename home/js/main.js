fb93562462ed6ff79f26ec283f6bcc5f6d67387a
hixiaochen <488675848@qq.com>
2020-08-17 20:54:29 +0800

    Site updated: 2020-08-17 20:54:29

--- home/js/main.js
+++ home/js/main.js
@@ -1,110 +1,198 @@
-jQuery(document).ready(function($) {
-    //check if background-images have been loaded and show list items
-    $('.projects-container li').bgLoaded({
-        afterLoaded: function() {
-            showCaption($('.projects-container li').eq(0));
-        }
-    });
-
-    //open project
-    $('.projects-container li').on('click', function() {
-        var selectedProject = $(this),
-                toggle = !selectedProject.hasClass('is-full-width');
-        if (toggle)
-            toggleProject($(this), $('.projects-container'), toggle);
-    });
-
-    //close project
-    $('.projects-container .cd-close').on('click', function() {
-        toggleProject($('.is-full-width'), $('.projects-container'), false);
-    });
-
-    //scroll to project info
-    $('.projects-container .cd-scroll').on('click', function() {
-        $('body,html').animate({'scrollTop': $(window).height()}, 500);
-    });
-
-    //update title and .cd-scroll opacity while scrolling
-    $(window).on('scroll', function() {
-        window.requestAnimationFrame(changeOpacity);
-    });
-
-    function toggleProject(project, container, bool) {
-        if (bool) {
-            //expand project
-            container.addClass('project-is-open');
-            project.addClass('is-full-width').siblings('li').removeClass('is-loaded');
-        } else {
-            //check media query
-            var mq = window.getComputedStyle(document.querySelector('.projects-container'), '::before').getPropertyValue('content'),
-                    delay = (mq == 'mobile') ? 100 : 0;
-
-            container.removeClass('project-is-open');
-            //fade out project
-            project.animate({opacity: 0}, 800, function() {
-                project.removeClass('is-loaded');
-                $('.projects-container').find('.cd-scroll').attr('style', '');
-                setTimeout(function() {
-                    project.attr('style', '').removeClass('is-full-width').find('.cd-title').attr('style', '');
-                }, delay);
-                setTimeout(function() {
-                    showCaption($('.projects-container li').eq(0));
-                }, 300);
-            });
-        }
-    }
-
-    function changeOpacity() {
-        var newOpacity = 1 - $(window).scrollTop() / 300;
-        $('.projects-container .cd-scroll').css('opacity', newOpacity);
-        $('.is-full-width .cd-title').css('opacity', newOpacity);
-    }
-
-    function showCaption(project) {
-        if (project.length > 0) {
-            setTimeout(function() {
-                project.addClass('is-loaded');
-                showCaption(project.next());
-            }, 150);
-        }
-    }
-});
-
-/*
- * BG Loaded
- * Copyright (c) 2014 Jonathan Catmull
- * Licensed under the MIT license.
- */
 (function($) {
-    $.fn.bgLoaded = function(custom) {
-        var self = this;
+  "use strict";
+  $(window).on("load", function() { // makes sure the whole site is loaded
+    //preloader
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+    
+    //masonry
+    $('.grid').masonry({
+      itemSelector: '.grid-item'
+      
+    });    
+  });
 
-        // Default plugin settings
-        var defaults = {
-            afterLoaded: function() {
-                this.addClass('bg-loaded');
+
+  $(document).ready(function(){  
+
+    //active menu
+    $(document).on("scroll", onScroll);
+ 
+    $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+ 
+      $('a').each(function () {
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+ 
+      var target = this.hash;
+      $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+      });
+    });
+
+    
+    //scroll js
+    smoothScroll.init({
+      selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+      selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+      speed: 500, // Integer. How fast to complete the scroll in milliseconds
+      easing: 'easeInOutCubic', // Easing pattern to use
+      updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+      offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+      callback: function ( toggle, anchor ) {} // Function to run after scrolling
+    });
+
+    //menu
+    var bodyEl = document.body,
+    content = document.querySelector( '.content-wrap' ),
+    openbtn = document.getElementById( 'open-button' ),
+    closebtn = document.getElementById( 'close-button' ),
+    isOpen = false;
+
+    function inits() {
+      initEvents();
+    }
+
+    function initEvents() {
+      openbtn.addEventListener( 'click', toggleMenu );
+      if( closebtn ) {
+        closebtn.addEventListener( 'click', toggleMenu );
+      }
+
+      // close the menu element if the target it´s not the menu element or one of its descendants..
+      content.addEventListener( 'click', function(ev) {
+        var target = ev.target;
+        if( isOpen && target !== openbtn ) {
+          toggleMenu();
+        }
+      } );
+    }
+
+    function toggleMenu() {
+      if( isOpen ) {
+        classie.remove( bodyEl, 'show-menu' );
+      }
+      else {
+        classie.add( bodyEl, 'show-menu' );
+      }
+      isOpen = !isOpen;
+    }
+
+    inits();
+
+
+    //typed js
+    $(".typed").typed({
+        strings: ["My Name is Xiaochen", "我的名字叫小陈", "欢迎来到我的主页"],
+        typeSpeed: 100,
+        backDelay: 900,
+        // loop
+        loop: true
+    });
+
+    //owl carousel
+    $('.owl-carousel').owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet : [768,1],
+      itemsMobile : [479,1],
+
+      // CSS Styles
+      baseClass : "owl-carousel",
+      theme : "owl-theme"
+    });
+
+    $('.owl-carousel2').owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet : [768,1],
+      itemsMobile : [479,1],
+      autoPlay : false,
+
+      // CSS Styles
+      baseClass : "owl-carousel",
+      theme : "owl-theme"
+    });
+
+    //contact
+    $('input').blur(function() {
+
+      // check if the input has any value (if we've typed into it)
+      if ($(this).val())
+        $(this).addClass('used');
+      else
+        $(this).removeClass('used');
+    });
+
+    //pop up porfolio
+    $('.portfolio-image li a').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+      // other options
+    });
+    
+    //Skill
+    jQuery('.skillbar').each(function() {
+      jQuery(this).appear(function() {
+        jQuery(this).find('.count-bar').animate({
+          width:jQuery(this).attr('data-percent')
+        },3000);
+        var percent = jQuery(this).attr('data-percent');
+        jQuery(this).find('.count').html('<span>' + percent + '</span>');
+      });
+    }); 
+
+  
+  });
+  
+    
+  //header
+  function inits() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector(".for-sticky");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"opacity-nav");
+        } else {
+            if (classie.has(header,"opacity-nav")) {
+                classie.remove(header,"opacity-nav");
             }
-        };
+          }
+      });
+    }
 
-        // Merge default and user settings
-        var settings = $.extend({}, defaults, custom);
+  window.onload = inits();
 
-        // Loop through element
-        self.each(function() {
-            var $this = $(this),
-                    bgImgs = window.getComputedStyle($this.get(0), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "").split(', ');
-            $this.data('loaded-count', 0);
-            $.each(bgImgs, function(key, value) {
-                var img = value.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
-                $('<img/>').attr('src', img).load(function() {
-                    $(this).remove(); // prevent memory leaks
-                    $this.data('loaded-count', $this.data('loaded-count') + 1);
-                    if ($this.data('loaded-count') >= bgImgs.length) {
-                        settings.afterLoaded.call($this);
-                    }
-                });
-            });
+  //nav-active
+  function onScroll(event){
+    var scrollPosition = $(document).scrollTop();
+    $('.menu-list a').each(function () {
+      var currentLink = $(this);
+      var refElement = $(currentLink.attr("href"));
+      if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+        $('.menu-list a').removeClass("active");
+        currentLink.addClass("active");
+      }
+      else{
+        currentLink.removeClass("active");
+      }
+    });
+  }
 
-        });
-    };
 })(jQuery);
